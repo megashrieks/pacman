9eef521c0c768587033085bac520bc27d62de2e2
megashrieks <shrikanthbudya@gmail.com>
2018-07-03 23:28:35 +0530

    drawing grids now

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,37 @@ function resize() {
 	can.height = window.innerHeight;
 	clear();
 }
+var vel = 2;
+var speed = {
+	x: vel,
+	y: 0
+};
+window.onkeydown = function(e) {
+	switch (e.keyCode) {
+		case 37:
+			face["CURRENT"] = face.LEFT;
+			speed = {
+				x: -vel,
+				y: 0
+			};
+			break;
+		case 38:
+			face["CURRENT"] = face.UP;
+			speed = { x: 0, y: -vel };
+			break;
+		case 39:
+			face["CURRENT"] = face.RIGHT;
+			speed = { x: vel, y: 0 };
+			break;
+		case 40:
+			face["CURRENT"] = face.DOWN;
+			speed = { x: 0, y: vel };
+			break;
+		default:
+			face["CURRENT"] = face["CURRENT"];
+			break;
+	}
+};
 resize();
 window.onresize = resize;
 var pacman = {
@@ -36,16 +67,54 @@ var face = {
 		final: Math.PI * 2 + Math.PI / 2
 	}
 };
-face["CURRENT"] = face.LEFT;
+face["CURRENT"] = face.RIGHT;
 var mouth = {
 	direction: 1,
 	wide: 1,
 	speed: 0.1,
-	max: Math.PI * 0.8,
+	max: Math.PI * 0.6,
 	min: Math.PI * 0.05
+};
+var grid = []
+var padding = 15;
+function createGrid() {
+	var width = ~~(can.width / (radius*2 + padding));
+	var height = ~~(can.height / (radius * 2 + padding));
+	for (var i = 0; i < height; ++i) {
+		let temp = [];
+		for (var j = 0; j < width; ++j) {
+			temp.push(~~(Math.random() * 3));
+		}
+		grid.push(temp);
+	}
+	console.log(width == grid.length);
+}
+function drawMaze() {
+	for (var i = 1; i < grid.length+1; ++i){
+		for (var j = 1; j < grid[i-1].length+1; ++j){
+			if (grid[i-1][j-1] === 0) continue;
+			ctx.beginPath();
+			if (grid[i-1][j-1] === 1) {
+				let y = i * (radius * 2 + padding);
+				let x = j * (radius * 2 + padding);
+				ctx.moveTo(x, y);
+				ctx.lineTo(x + (radius * 2 + padding), y);
+			}
+			if (grid[i-1][j-1] === 2) {
+				let y = i * (radius * 2 + padding);
+				let x = j * (radius * 2 + padding);
+				ctx.moveTo(x, y);
+				ctx.lineTo(x, y + (radius * 2 + padding));
+			}
+			ctx.closePath();
+			ctx.strokeStyle = "#fff";
+			ctx.stroke();
+		}
+	}
 }
 function draw() {
 	clear();
+	drawMaze();
 	ctx.beginPath();
 	ctx.strokeStyle = "yellow";
 	if (mouth.wide > mouth.max || mouth.wide < mouth.min) mouth.direction *= -1;
@@ -58,6 +127,58 @@ function draw() {
 	);
 	mouth.wide += mouth.speed * mouth.direction;
 	ctx.stroke();
+	ctx.closePath();
+	if (!(pacman.x >= can.width - radius ||
+		pacman.x <= radius ||
+		pacman.y >= can.height - radius ||
+		pacman.y <= radius)
+	) {
+		pacman.x += speed.x;
+		pacman.y += speed.y;
+	} else {
+		speed = {
+			x: 0,
+			y: 0
+		};
+		if (pacman.x >= can.width - radius) {
+			pacman.x = can.width - (radius+1);
+		} else if (pacman.x <= radius) {
+			pacman.x = radius+1;
+		} else if (pacman.y >= can.height - radius) {
+			pacman.y = can.height - (radius + 1);
+		} else if (pacman.y <= radius) {
+			pacman.y = radius + 1;
+		}
+	}
+	let current = face.CURRENT;
+	if (current == face.UP) {
+		current = face.DOWN;
+	} else if (current == face.DOWN) {
+		current = face.UP;
+	}
+
+	ctx.beginPath();
+	ctx.moveTo(
+		pacman.x +
+			Math.sin(current.initial + mouth.wide / 2 + Math.PI / 2) * radius,
+		pacman.y +
+			Math.cos(current.initial + mouth.wide / 2 + Math.PI / 2) * radius
+	);
+	ctx.lineTo(pacman.x, pacman.y);
+	ctx.stroke();
+	ctx.closePath();
+	ctx.beginPath();
+	ctx.moveTo(pacman.x, pacman.y);
+	ctx.lineTo(
+		pacman.x +
+			Math.sin(current.final - mouth.wide / 2 + Math.PI / 2) * radius,
+		pacman.y +
+			Math.cos(current.final - mouth.wide / 2 + Math.PI / 2) * radius
+	);
+	ctx.stroke();
+	ctx.closePath();
+
 	requestAnimationFrame(draw);
 }
+createGrid();
 draw();
